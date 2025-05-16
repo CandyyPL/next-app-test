d5a458460f85bc5aecdffa17632dd9df58ce17e0
CandyyPL <aplay58.yt@gmail.com>
2025-05-16 20:15:54 +0200

    Add Auth.js auth
    Added NextAuth to the project with GitHub provider

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+// @type {import("prettier").Config}
+
+const config = {
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  tabWidth: 2,
+  useTabs: false,
+  bracketSpacing: true,
+}
+
+export default config
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { auth, signIn, signOut } from '@/auth'
+
+const Navbar = async () => {
+  const session = await auth()
+
+  return (
+    <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+      <nav className='flex justify-between items-center'>
+        <Link href='/' className='flex justify-between items-center'>
+          <Image
+            src='/youtube.png'
+            alt='logo'
+            width={50}
+            height={50}
+            className='pr-2'
+          />
+          <span className='text-xl'>iSmasTube</span>
+        </Link>
+
+        <div className='flex items-center gap-5 font-black'>
+          {session && session?.user ? (
+            <>
+              <Link href='/startup/create'>
+                <span>Create</span>
+              </Link>
+
+              <form
+                action={async () => {
+                  'use server'
+                  await signOut({ redirectTo: '/' })
+                }}
+              >
+                <button type='submit' className='cursor-pointer'>
+                  Logout
+                </button>
+              </form>
+
+              <Link href={`/users/${session?.user?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <>
+              <form
+                action={async () => {
+                  'use server'
+                  await signIn('github')
+                }}
+              >
+                <button type='submit' className='cursor-pointer'>
+                  Login
+                </button>
+              </form>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
