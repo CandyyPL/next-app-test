c8f893262fcaece075beaa467ffb913574da6ac4
CandyyPL <aplay58.yt@gmail.com>
2025-05-17 14:00:15 +0200

    Create sanity schemas, add author & startup data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import React from 'react'
 import './globals.css'
 import localFont from 'next/font/local'
+import 'easymde/dist/easymde.min.css'
 
 const workSans = localFont({
   src: [
